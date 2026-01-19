9b52982b90d62e0703b3647f4bcfc47033f3c969
ApisegInnodata <150651011+ApisegInnodata@users.noreply.github.com>
2026-01-19 12:03:57 +0530

    Update version retrieval from pyproject.toml

--- sherlock_project/__init__.py
+++ sherlock_project/__init__.py
@@ -18,7 +18,7 @@ def get_version() -> str:
         pyproject_path: pathlib.Path = pathlib.Path(__file__).resolve().parent.parent / "pyproject.toml"
         with pyproject_path.open("rb") as f:
             pyproject_data = tomli.load(f)
-        return pyproject_data["tool"]["poetry"]["version"]
+        return pyproject_data["project"]["version"]
 
 # This variable is only used to check for ImportErrors induced by users running as script rather than as module or package
 import_error_test_var = None
